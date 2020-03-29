ef83189852bc11a0c5e61b4f1d2a900a517ea5cf
seferonth <seferonth@gmail.com>
2020-03-29 17:13:47 -0500

    Start of new Code, Getting command line arguments

--- CompArchWork.py
+++ CompArchWork.py
@@ -1,1 +1,11 @@
 
+import sys
+
+
+
+#simple thing to grab command line arguments
+print("Starting project \n")
+for i in sys.argv:
+    print(i, '\n')
+    sys.stdout.write(i, '\n')
+print(sys.argv[0])
